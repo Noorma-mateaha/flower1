ab2149030b522db10b3666aa3634f38b60ced602
Noorma-mateaha <80210646+Noorma-mateaha@users.noreply.github.com>
2021-03-07 23:15:11 +0700

    Add files via upload

---
+++ css/test.php
@@ -0,0 +1,56 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Mali|Marmelad&display=swap" rel="stylesheet">
+    <title>ร้านสหายอลูมิเนียมกระจก</title>
+    <script>
+    $(document).ready(function() {
+
+    });
+    </script>
+
+    <link rel="stylesheet" href="test.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="content">
+            <h1>ABCD</h1>
+            <p>content</p>
+        </div>
+
+        <div class="form">
+            <div class="top-form">
+                <div class="inner-form">
+                    <div class="label">name</div>
+                    <input type="text" placeholder="Name">
+                </div>
+                <div class="inner-form">
+                    <div class="label">email</div>
+                    <input type="text" placeholder="Email">
+                </div>
+                <div class="inner-form">
+                    <div class="label">phone</div>
+                    <input type="text" placeholder="Phone">
+                </div>
+            </div>
+
+            <div class="middle-form">
+                <div class="inner-form">
+                    <div class="label">subject</div>
+                    <input type="text" placeholder="Subject">
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="inner-form">
+                    <div class="label">Your message</div>
+                    <input type="text" placeholder="Message">
+                </div>
+            </div><br>
+
+            <div class="btn">send form</div>
+        </div>
+</body>
+
+</html>
